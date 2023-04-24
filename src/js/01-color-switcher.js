001e0ac4b0c23426d31c89e434fa31dfd77582fb
vladaorlik <vladaorlik@users.noreply.github.com>
2023-04-24 19:07:06 +0300

    Color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+const bodyEl = document.body;
+let switcherTimerId = null;
 
+startButton.addEventListener('click', onStartBtnClick);
+stopButton.addEventListener('click', onStopBtnClick);
+stopButton.setAttribute('disabled', 'true');
+
+function onStartBtnClick() {    
+switcherTimerId = setInterval(changeColor, 1000);
+startButton.setAttribute('disabled', 'true');   
+stopButton.removeAttribute('disabled');
+    
+}
+
+function changeColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
+
+function onStopBtnClick() {
+    startButton.removeAttribute('disabled');
+    clearInterval(switcherTimerId);
+    stopButton.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
